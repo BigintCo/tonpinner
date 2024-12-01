0aabe9d131410117be94a418aac1e564a2842d76
TheMMT <merttekdemir3@gmail.com>
2024-12-01 16:10:18 +0300

    Update page.tsx

--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -18,13 +18,59 @@ import LayoutWrapper from "@/layout";
 import { useAppContext } from "@/providers/app-provider";
 import ApiService from "@/utils/api-service";
 import { toast } from "react-toastify";
+import { MapProvider } from "@/providers/MapProvider";
+import { MapComponent } from "@/components/map";
 
 
 export default function Home() {
   type IPost = {
     content: string;
     photoURI: string;
-  };
+    place: string;
+  }
+  interface Place {
+    business_status: string;
+    geometry: {
+      location: {
+        lat: number;
+        lng: number;
+      };
+      viewport: {
+        northeast: {
+          lat: number;
+          lng: number;
+        };
+        southwest: {
+          lat: number;
+          lng: number;
+        };
+      };
+    };
+    icon: string;
+    icon_background_color: string;
+    icon_mask_base_uri: string;
+    name: string;
+    opening_hours?: {
+      open_now: boolean;
+    };
+    photos?: {
+      height: number;
+      html_attributions: string[];
+      photo_reference: string;
+      width: number;
+    }[];
+    place_id: string;
+    plus_code?: {
+      compound_code: string;
+      global_code: string;
+    };
+    rating?: number;
+    reference: string;
+    scope: string;
+    types: string[];
+    user_ratings_total?: number;
+    vicinity: string;
+  }
   const { user } = useAppContext();
   const [openMenu, setOpenMenu] = useState(false);
   const [section, setSection] = useState<number>(0);
@@ -49,8 +95,11 @@ export default function Home() {
     setLoading(false);
   }
   useEffect(() => {
-    getDiscoverPosts()
+    getDiscoverPosts();
   }, []);
+  useEffect(() => {
+    console.log(posts, 'posts');
+  }, [posts]);
   return (
     <LayoutWrapper>
       <div className="w-full h-screen flex flex-col items-start relative bg-white">
@@ -76,7 +125,7 @@ export default function Home() {
           </div>
         </div>
         <div className="w-full h-[90vh] overflow-scroll scroll-hidden  relative flex flex-col justify-start items-start gap-2">
-          <div className="w-full grid grid-cols-12 justify-center items-center text-sm sticky top-0">
+          <div className="w-full grid grid-cols-12 justify-center items-center text-sm sticky top-0 z-50">
             <div className="col-span-6">
               <button onClick={() => { setSection(0) }} className={`${section === 0 ? 'border-[#24A1DE]' : ''} py-2 border-b   w-full flex justify-center items-center bg-white text-pinner`}>
                 Discover
@@ -88,15 +137,64 @@ export default function Home() {
               </button>
             </div>
           </div>
-          <div className="w-full grid grid-cols-12 justify-start items-start gap-1">
-            {
-              posts.map((post, index) => (
-                <div key={index} className="col-span-4">
-                  <img alt="image" className="w-full aspect-square" src={post.photoURI}></img>
-                </div>
-              ))
-            }
+          <div className="w-full  px-8 py-4 rounded-lg flex flex-col justify-start items-start gap-3">
+            <div className="w-full">
+              <MapProvider>
+                <MapComponent />
+              </MapProvider>
+            </div>
+            <div className="w-full flex justify-between items-center ">
+              <button className="rounded-full bg-[#24A1DE] p-2 flex justify-center items-center text-xs text-white">
+                282 visited
+              </button>
+              <button className="rounded-full bg-[#24A1DE] p-2 flex justify-center items-center text-xs text-white">
+                0 saved
+              </button>
+              <button className="rounded-full bg-[#24A1DE] p-2 flex justify-center items-center text-xs text-white">
+                36/100 Categories
+              </button>
+            </div>
+            {/* <div className="w-full flex justify-start items-center gap-2 px-8 py-1 text-xs  border-t border-b border-gray-600/10">
+              <span className="text-pinner font-bold">400 </span> Check-ins
+            </div> */}
+            <div className="w-full flex flex-col justify-start items-start gap-6 py-4">
+              {posts &&
+                posts.map((post, index) => {
+                  const place: Place = typeof post.place === "string" ? JSON.parse(post.place) : post.place;
+                  return (
 
+                    <div key={index} className="w-full flex flex-col justify-start items-start gap-4">
+                      <div className="text-sm bg-blue-600/10 p-2 rounded-xl text-blue-500">Today</div>
+                      <div className="w-full flex justify-start items-start gap-5">
+                        <div className="w-[15%] rounded-full bg-pinner p-3 flex justify-center items-center">
+                          <Image src={place.icon} alt="" width={20} height={20} className="w-5 h-5" />
+                        </div>
+                        <div className="w-[85%] flex flex-col justify-start items-start gap-2">
+                          <span className="text-sm font-medium">{place.name}</span>
+                          <div className="w-full">
+                            <img src={post.photoURI} className="object-contain" alt=""></img>
+                          </div>
+                          <div className="flex flex-col justify-start items-start gap-1">
+                            <span className="text-xs text-gray-400">Today</span>
+                          </div>
+                          <span className="text-xs text-gray-400 flex justify-start items-center gap-1">Checked by <span className="text-pinner">Today</span></span>
+                          {/* <div className="w-full flex justify-start items-center gap-1">
+                          {
+                            place.humans.map((human, index) => (
+                              <div key={index} className="w-10 aspect-square rounded-full border-2 border-white">
+                                <Image alt="pp" src={human.image} className="w-full aspect-square rounded-full" />
+                              </div>
+                            ))
+                          }
+                        </div> */}
+                        </div>
+                      </div>
+                    </div>
+                  )
+
+                })
+              }
+            </div>
           </div>
         </div>
         <div className="w-full flex justify-between gap-10 items-end px-16 py-3 relative border-t border-[#24A1DE]/30">
